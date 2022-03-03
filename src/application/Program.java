74bf7f054ba45ccdbdac3be0fcb205de9e03edd4
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-03-02 23:25:10 -0300

    Insert

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,24 +11,28 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerdao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("==========TESTE 1: seller findById=========== ");
 		Seller seller = sellerdao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n==========TESTE 2:seller FindByDepartment==========");
 		Department department = new Department(2, null);
-		List<Seller> list =  sellerdao.findByDepartment(department);
-		for(Seller obj : list) 
+		List<Seller> list = sellerdao.findByDepartment(department);
+		for (Seller obj : list)
 			System.out.println(obj);
-		
+
 		System.out.println("\n==========TESTE 3:seller FindAll==========");
-		list =  sellerdao.findAll();
-		for(Seller obj : list) 
+		list = sellerdao.findAll();
+		for (Seller obj : list)
 			System.out.println(obj);
-		
+
+		System.out.println("\n==========TESTE 3:seller INSERT==========");
+		Seller newSeller = new Seller(null, "Greg", "gref@gmail.com", new Date(), 4000.0, department);
+		SellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 
 }
--- src/model/dao/impl/SellerDaoJdbc.java
+++ src/model/dao/impl/SellerDaoJdbc.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,9 +24,34 @@ public class SellerDaoJdbc implements SellerDao {
 
 	}
 
-	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller " + "(Name, Email, BirthDate, BaseSalary,DepartmentId) "
+					+ "VALUES " + "(?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+
+			int rowsAffected = st.executeUpdate();
+
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setId(id);
+				}
+				DB.closeResultSet(rs);
+			} else {
+				throw new DbException("unexpected error! no rows are affected");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
@@ -34,25 +60,25 @@ public class SellerDaoJdbc implements SellerDao {
 		// TODO Auto-generated method stub
 
 	}
+
 	@Override
 	public List<Seller> findAll() {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT seller.*,department.Name AS DepName "+ 
-					"FROM seller INNER JOIN department "+
-					"ON seller.DepartmentId = department.Id "+
-					"ORDER BY Name ");
-			
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name AS DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name ");
+
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			while (rs.next()) {
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
-				if(dep == null) {
+
+				if (dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
@@ -71,7 +97,7 @@ public class SellerDaoJdbc implements SellerDao {
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -99,18 +125,17 @@ public class SellerDaoJdbc implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 	}
-	
-	
-	//metodo para instaciar seller, usado em find by id
+
+	// metodo para instaciar seller, usado em find by id
 	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
-			Seller obj = new Seller();
-			obj.setId(rs.getInt("Id"));
-			obj.setName(rs.getString("Name"));
-			obj.setEmail(rs.getString("Email"));
-			obj.setBaseSalary(rs.getDouble("BaseSalary"));
-			obj.setBirthDate(rs.getDate("BirthDate"));
-			obj.setDepartment(dep);
-			return obj;
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
 	}
 
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
@@ -125,22 +150,20 @@ public class SellerDaoJdbc implements SellerDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT seller.*,department.Name AS DepName "+ 
-					"FROM seller INNER JOIN department "+
-					"ON seller.DepartmentId = department.Id "+
-					"WHERE DepartmentId = ? "+
-					"ORDER BY Name ");
-			
-			st.setInt(1,department.getId() );
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name AS DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name ");
+
+			st.setInt(1, department.getId());
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			while (rs.next()) {
 				Department dep = map.get(rs.getInt("DepartmentId"));
-				
-				if(dep == null) {
+
+				if (dep == null) {
 					dep = instantiateDepartment(rs);
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
@@ -155,5 +178,5 @@ public class SellerDaoJdbc implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 
-}
+	}
 }
